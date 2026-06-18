bfedda2d3eb16df3204e42e66f5e5e247ffbd7ee
Ananya <ananyadixit1816@gmail.com>
2026-06-19 01:34:13 +0530

    Add User registration with BCrypt password encryption and Spring Security config

---
+++ src/main/java/com/ecommerce/ecommerce/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.ecommerce.ecommerce.controller;
+
+import com.ecommerce.ecommerce.model.User;
+import com.ecommerce.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
---
+++ src/main/java/com/ecommerce/ecommerce/service/UserService.java
@@ -0,0 +1,21 @@
+package com.ecommerce.ecommerce.service;
+
+import com.ecommerce.ecommerce.model.User;
+import com.ecommerce.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
